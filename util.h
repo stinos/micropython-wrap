a212b61d008b80f574d84056a2e4880c901fc9c3
stijn <stijn@ignitron.net>
2016-01-12 17:12:28 +0100

    Add function which formats uPy exception into std::string

---
+++ util.h
@@ -0,0 +1,17 @@
+#ifndef MICROPYTHON_WRAP_UTIL_H
+#define MICROPYTHON_WRAP_UTIL_H
+
+#include "detail/micropython.h"
+
+namespace upywrap
+{
+  inline std::string ExceptionToString( mp_obj_t ex )
+  {
+    std::string exMessage;
+    const mp_print_t mp_my_print{ &exMessage, [] ( void* data, const char* str, mp_uint_t len ) { ( (std::string*) data )->append( str, len ); } };
+    mp_obj_print_exception( &mp_my_print, ex );
+    return exMessage;
+  }
+}
+
+#endif //#ifndef MICROPYTHON_WRAP_UTIL_H
