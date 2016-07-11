64d0d495f19cf3b2e071001b0197758dbf53d7fc
stijn <stijn@ignitron.net>
2016-07-11 16:57:52 +0200

    Add function for getting a variable's string representation

--- util.h
+++ util.h
@@ -5,12 +5,25 @@
 
 namespace upywrap
 {
+  inline mp_print_t PrintToString( std::string& dest )
+  {
+    return mp_print_t{ &dest, [] ( void* data, const char* str, mp_uint_t len ) { ( (std::string*) data )->append( str, len ); } };
+  }
+
   inline std::string ExceptionToString( mp_obj_t ex )
   {
     std::string exMessage;
-    const mp_print_t mp_my_print{ &exMessage, [] ( void* data, const char* str, mp_uint_t len ) { ( (std::string*) data )->append( str, len ); } };
+    const auto mp_my_print( PrintToString( exMessage ) );
     mp_obj_print_exception( &mp_my_print, ex );
     return exMessage;
+  }
+
+  inline std::string VariableValueToString( mp_obj_t obj, mp_print_kind_t kind = PRINT_REPR )
+  {
+    std::string var;
+    const auto mp_my_print( PrintToString( var ) );
+    mp_obj_print_helper( &mp_my_print, obj, kind );
+    return var;
   }
 }
 
