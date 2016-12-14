23f8d3441788bd8818bde5c7afeadbc63f987393
stijn <stijn@ignitron.net>
2016-12-14 12:09:53 +0100

    tests: Modify finalizer test so there's more chance it passes
    Current linux builds don't always collect the Context instance: invoking the
    test file directly on the command line is ok, but when ran via run-tests the
    finalizer isn't called due to something in the gc... Adding two variables fixes
    this for now.

--- tests/py/finalizer.py
+++ tests/py/finalizer.py
@@ -1,6 +1,11 @@
 import upywraptest
 import gc
 
+# gc isn't entirely deterministic and without these two lines
+# the linux build doesn't collect x, though arguably it should
+a = 1
+b = 2
+
 def fun() :
   x = upywraptest.Context()
 
