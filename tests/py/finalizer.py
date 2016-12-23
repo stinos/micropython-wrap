e26e6957a118c6f69e74a1b480524106616a5c52
stijn <stinos@zoho.com>
2016-12-23 13:07:12 +0100

    tests: Adjust finalizer test once more
    Tests run fine on mu machine, not on Travie, so add some random variables.

--- tests/py/finalizer.py
+++ tests/py/finalizer.py
@@ -1,10 +1,12 @@
 import upywraptest
 import gc
 
-# gc isn't entirely deterministic and without these two lines
+# gc isn't entirely deterministic and without these lines
 # the linux build doesn't collect x, though arguably it should
 a = 1
 b = 2
+c = 3
+d = [a] * 13
 
 def fun() :
   x = upywraptest.Context()
