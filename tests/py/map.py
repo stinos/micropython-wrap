0690f839f87cab26a4232d7babf90b8a1bcf5267
stijn <stijn@ignitron.net>
2024-03-07 14:46:46 +0100

    tests: Fix test depending on non-deterministic dictionary order

--- tests/py/map.py
+++ tests/py/map.py
@@ -1,4 +1,6 @@
 import upywraptest
 
-print(upywraptest.Map1({'a': 1, 'b': 2, 'def': 444}))
-print(upywraptest.Map2({'a': [1], 'b': [2]}))
+m = upywraptest.Map1({"a": 1, "b": 2, "def": 444})
+print([(k, m[k]) for k in sorted(m.keys())])
+m = upywraptest.Map2({"a": [1], "b": [2]})
+print([(k, m[k]) for k in sorted(m.keys())])
